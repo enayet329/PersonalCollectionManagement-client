21be5211a5208b5c845f7754e3f2353afa4d17fc
enayet329 <mdsakayethossen2@gmail.com>
2024-08-06 14:20:10 +0600

    Delete unnessesary file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,12 @@ import { AdminDashboardComponent } from './features/admin/admin-dashboard/admin-
 import { CollectionListComponent } from './features/collections/collection-list/collection-list.component';
 import { ItemListComponent } from './features/items/item-list/item-list.component';
 import { ProfileViewComponent } from './features/user-profile/profile-view/profile-view.component';
-import { HomeModule } from './features/home/home.module';
+
+
 export const routes: Routes = [
-  { path: '', component: HomeModule },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'collections', component: CollectionListComponent, canActivate: [AuthGuard] },
-  { path: 'items', component: ItemListComponent, canActivate: [AuthGuard] },
-  { path: 'profile', component: ProfileViewComponent, canActivate: [AuthGuard] },
-  { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard] },
-  // Add more routes as needed
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch:'full'
+  }
 ];
