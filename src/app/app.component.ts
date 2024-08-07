9ae47ca95aa411db686a833e0ab1b2f8a9946344
enayet329 <mdsakayethossen2@gmail.com>
2024-08-07 10:00:02 +0600

    Delete shanred file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AdminDashboardComponent } from "./features/admin/admin-dashboard/admin-dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminDashboardComponent],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'PersonalCollectionManagement-client';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,5 @@
 import { Routes } from '@angular/router';
-import { AuthGuard } from './core/guards/auth.guard';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
-import { AdminDashboardComponent } from './features/admin/admin-dashboard/admin-dashboard.component';
-import { CollectionListComponent } from './features/collections/collection-list/collection-list.component';
-import { ItemListComponent } from './features/items/item-list/item-list.component';
-import { ProfileViewComponent } from './features/user-profile/profile-view/profile-view.component';
+
 
 
 export const routes: Routes = [
@@ -14,4 +8,5 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch:'full'
   }
+  
 ];
