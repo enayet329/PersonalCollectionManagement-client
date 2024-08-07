5a39e9657f55891a43de9053ab4d59144af72ee5
enayet329 <mdsakayethossen2@gmail.com>
2024-08-07 22:52:48 +0600

    Update app route component set route path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { AboutComponent } from './shared/about/about.component';
+import { ContactComponent } from './shared/contact/contact.component';
 
 
 
@@ -7,6 +10,17 @@ export const routes: Routes = [
     path:'',
     redirectTo: 'home',
     pathMatch:'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
   }
-  
 ];
