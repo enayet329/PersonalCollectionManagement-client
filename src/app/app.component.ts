96c5d40ab6ed0ef02af1e51f2d15b5aa2f08aa35
enayet329 <mdsakayethossen2@gmail.com>
2024-08-07 22:51:07 +0600

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { AboutComponent } from './shared/about/about.component';
+import { ContactComponent } from './shared/contact/contact.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-  ],
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PersonalCollectionManagement-client';
