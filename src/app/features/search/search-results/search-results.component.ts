1d135c9244c3673a6778c15128b7a59ff5a45564
enayet329 <mdsakayethossen2@gmail.com>
2024-08-09 00:22:10 +0600

    feat(search-results): Add search results page and feature - Import CommonModule and RouterModule for component functionality - Fetch collections from searchService  on component initialization - Define template and styles for the component

--- src/app/features/search/search-results/search-results.component.ts
+++ src/app/features/search/search-results/search-results.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Item } from '../../../core/model/item.model';
+import { SearchService } from '../../../core/services/search.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './search-results.component.html',
-  styleUrl: './search-results.component.css'
+  styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent {
+  items: Item[] = [];
+  query: string = '';
 
+  constructor(
+    private searchService: SearchService,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.query = params['query'];
+      this.onSearch();
+    });
+  }
+
+  onSearch() {
+    if (this.query.trim()) {
+      this.searchService.search(this.query).subscribe(items => {
+        this.items = items;
+      });
+    }
+  }
+
+  getLimitedTags(tags: string[], limit: number): string[] {
+    return tags.slice(0, limit);
+  }
 }
