eeeac060656d35d1155dab327ce0b0c4c8b9260d
enayet329 <mdsakayethossen2@gmail.com>
2024-08-09 00:29:04 +0600

    feat(app-component): inject ThemeService for dark mode support
    - Add darkModeService property to AppComponent
    - Inject ThemeService using Angular's inject function for dark mode functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 import { AboutComponent } from './shared/about/about.component';
 import { ContactComponent } from './shared/contact/contact.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { routes } from './app.routes';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +17,11 @@ import { routes } from './app.routes';
     ContactComponent,
     NavbarComponent,
     FooterComponent,
-],
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PersonalCollectionManagement-client';
+  darkModeService: ThemeService = inject(ThemeService);
 }
