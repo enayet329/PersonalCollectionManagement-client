5760acd2988cebf780b3cad8f558e13184deaaea
enayet329 <mdsakayethossen2@gmail.com>
2024-08-08 14:50:01 +0600

    Feat: Add item model interface

---
+++ src/app/core/model/item.model.ts
@@ -0,0 +1,10 @@
+export interface Item {
+  id: string;
+  name: string;
+  description: string;
+  imgUrl: string;
+  dateAdded: string;
+  collectionName: string;
+  collectionId: string;
+  tagNames: string[];
+}
