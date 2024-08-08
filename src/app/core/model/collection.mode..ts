01206a3ce004ee08f36ded3702a882fbacc345f4
enayet329 <mdsakayethossen2@gmail.com>
2024-08-08 14:52:26 +0600

    feat: Add collection model interface

---
+++ src/app/core/model/collection.mode..ts
@@ -0,0 +1,13 @@
+
+export interface Collection {
+createdDate: string|number|Date;
+    id: string;
+    name: string;
+    description: string;
+    topic: string;
+    imageUrl: string;
+    userId: string;
+    userName: string;
+    itemCount: number;
+  }
+  
