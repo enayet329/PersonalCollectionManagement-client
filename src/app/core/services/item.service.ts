9b92d255f37ef8060cec4df59358b2df4787773d
enayet329 <mdsakayethossen2@gmail.com>
2024-08-08 14:51:57 +0600

    feat: Implement ItemService to fetch recent items
    - Adds ItemService with a method to get recent items from the API.
    - Uses HttpClient to make GET requests to the API endpoint defined in ItemConstants.
    - Provides an Observable of Item[] for consumers to subscribe to.

--- src/app/core/services/item.service.ts
+++ src/app/core/services/item.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ItemConstants } from '../constants/constants';
+import { Item } from '../model/item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
+  private apiUrl = ItemConstants.API_ENDPOINTS.GET_RECENT_ITEMS;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getRecentItems(): Observable<Item[]> {
+    return this.httpClient.get<Item[]>(this.apiUrl);
+  }
 }
