0467da37036d755b848410cb32b236e25fb3d181
enayet329 <mdsakayethossen2@gmail.com>
2024-08-08 23:22:59 +0600

    Add global theme searvice - Add function to change theme - Add function to update theme

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  darkModeSignal = signal<string>('null');
 
-  constructor() { }
+  constructor() {
+    const savedTheme = localStorage.getItem('theme') || 'null';
+    this.darkModeSignal.set(savedTheme);
+    this.applyTheme(savedTheme);
+  }
+
+  updateDarkMode() {
+    this.darkModeSignal.update((value) => {
+      const newTheme = value === 'dark' ? 'null' : 'dark';
+      this.applyTheme(newTheme);
+      return newTheme;
+    });
+  }
+
+  private applyTheme(theme: string) {
+    document.body.classList.toggle('dark-theme', theme === 'dark');
+    localStorage.setItem('theme', theme);
+  }
 }
