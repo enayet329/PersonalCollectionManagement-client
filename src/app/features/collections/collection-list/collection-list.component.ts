62373cf9839fdfaf269118995a49b2bb923e12e6
enayet329 <mdsakayethossen2@gmail.com>
2024-08-09 00:20:56 +0600

    feat (collection-list): add new feature - Import CommonModule and RouterModule for component functionality - Fetch collections from CollectionService on component initialization - Define template and styles for the component

--- src/app/features/collections/collection-list/collection-list.component.ts
+++ src/app/features/collections/collection-list/collection-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Collection } from '../../../core/model/collection.mode.';
+import { CollectionService } from '../../../core/services/collection.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-collection-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './collection-list.component.html',
-  styleUrl: './collection-list.component.css'
+  styleUrl: './collection-list.component.css',
 })
-export class CollectionListComponent {
+export class CollectionListComponent implements OnInit {
+  collections: Collection[] = [];
 
+  constructor(private collectionService: CollectionService) {}
+
+  ngOnInit(): void {
+    this.collectionService.getCollections().subscribe((data) => {
+      this.collections = data;
+    });
+  }
 }
