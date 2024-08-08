b642559db399e9d3bd379ff20b1f0defd4b801ac
enayet329 <mdsakayethossen2@gmail.com>
2024-08-08 14:54:24 +0600

    feat: Implement collectin service for featch largest collection

--- src/app/core/services/collection.service.ts
+++ src/app/core/services/collection.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Collection } from '../model/collection.mode.';
+import { CollectionConstants } from '../constants/constants';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CollectionService {
+  private apiUrl = CollectionConstants.API_ENDPOINTS.GET_COLLECTIONS;
 
-  constructor() { }
+  httpClient = inject(HttpClient);
+
+  getLatestCollections(): Observable<Collection[]> {
+    return this.httpClient.get<Collection[]>(this.apiUrl);
+  };
 }
