78286ee1d0eb4c10de16172395f7fc56a1618133
enayet329 <mdsakayethossen2@gmail.com>
2024-08-08 23:19:52 +0600

    Add full text search service - Algolia Search

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Item } from '../model/item.model';
+import { ItemConstants } from '../constants/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private apiUrl = ItemConstants.API_ENDPOINTS;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  search(query: string) {
+    const searchUrl = `${this.apiUrl.SEARCH_ITEMS}?query=${encodeURIComponent(query)}`;
+    return this.httpClient.get<Item[]>(searchUrl);
+  }
 }
