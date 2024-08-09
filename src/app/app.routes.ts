a0f3383fc62f8ac17a399afd01f70722dfa1d807
enayet329 <mdsakayethossen2@gmail.com>
2024-08-09 17:50:00 +0600

    feat: Add collection detail componenet route path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './shared/home/home.component';
 import { CollectionListComponent } from './features/collections/collection-list/collection-list.component';
 import { AdminDashboardComponent } from './features/admin/admin-dashboard/admin-dashboard.component';
 import { SearchResultsComponent } from './features/search/search-results/search-results.component';
+import { CollectionDetailComponent } from './features/collections/collection-detail/collection-detail.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'collection-list', component: CollectionListComponent },
   { path: 'dashboard', component: AdminDashboardComponent },
-  { path: 'search-results', component: SearchResultsComponent }
+  { path: 'search-results', component: SearchResultsComponent },
+  { path: 'collection-detail', component: CollectionDetailComponent }
 ];
