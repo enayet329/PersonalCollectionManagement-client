763aad9b1f8cfe072a1c3b366e6776f2af0d83d3
enayet329 <mdsakayethossen2@gmail.com>
2024-08-09 23:47:43 +0600

    feat: Add Collection-details and item-details routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { CollectionListComponent } from './features/collections/collection-list/
 import { AdminDashboardComponent } from './features/admin/admin-dashboard/admin-dashboard.component';
 import { SearchResultsComponent } from './features/search/search-results/search-results.component';
 import { CollectionDetailComponent } from './features/collections/collection-detail/collection-detail.component';
-
+import { ItemDetailComponent } from './features/items/item-detail/item-detail.component'; // Import ItemDetailComponent
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'collection-list', component: CollectionListComponent },
   { path: 'dashboard', component: AdminDashboardComponent },
   { path: 'search-results', component: SearchResultsComponent },
-  { path: 'collection-detail', component: CollectionDetailComponent }
+  { path: 'collection-detail/:id', component: CollectionDetailComponent },
+  { path: 'item-detail/:id', component: ItemDetailComponent },
 ];
