8f228838c2f8e4b5605a4155407b6af34a1be390
enayet329 <mdsakayethossen2@gmail.com>
2024-08-11 02:49:40 +0600

    feat: Implement createComment method in CommentService
    - Added createComment method to handle the creation of new comments.

--- src/app/core/services/comment.service.ts
+++ src/app/core/services/comment.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CommentConstants } from '../constants/constants';
 import { Observable } from 'rxjs';
+import { Comment, AddComment, CommentResponse } from '../model/comment.model'; // Import both Comment and AddComment
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,10 @@ export class CommentService {
     const url = this.apiUrl.GET_COMMENTS_BY_ITEM_ID;
     const params = { itemId: itemId };
     return this.httpClient.get<Comment[]>(url, { params: params });
+  }
+
+  createComment(comment: AddComment): Observable<CommentResponse> {
+    const url = this.apiUrl.ADD_COMMENT;
+    return this.httpClient.post<CommentResponse>(url, comment);
   }
 }
