ee8ada002b6f4087b976011e1c4e28e65177214e
enayet329 <mdsakayethossen2@gmail.com>
2024-08-10 12:36:09 +0600

    feat(commentservice): Add a function to featch comments associated with a particular item

--- src/app/core/services/comment.service.ts
+++ src/app/core/services/comment.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CommentConstants } from '../constants/constants';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  private apiUrl = CommentConstants.API_ENDPOINTS;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getCommetsByItemId(itemId: string): Observable<Comment[]> {
+    const url = this.apiUrl.GET_COMMENTS_BY_ITEM_ID;
+    const params = { itemId: itemId };
+    return this.httpClient.get<Comment[]>(url, { params: params });
+  }
 }
