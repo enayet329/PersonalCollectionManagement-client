67e629f44c78801b6727fcdcd4113622a014162d
enayet329 <mdsakayethossen2@gmail.com>
2024-08-10 12:43:22 +0600

    feat(like.service): Add a function to get like count of a item

--- src/app/core/services/like.service.ts
+++ src/app/core/services/like.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LikeConstants } from '../constants/constants';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LikeService {
+  private apiUrl = LikeConstants.API_ENDPOINTS;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getLikesByItemId(itemId: string): Observable<any> {
+    const url = this.apiUrl.GET_LIKE_BY_ITEM_ID;
+    const params = { itemId: itemId };
+    return this.httpClient.get(url, { params });
+  }
 }
