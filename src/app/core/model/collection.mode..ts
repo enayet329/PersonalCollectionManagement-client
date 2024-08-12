950c2b01b52060539ee0cc29a04b4486e914d078
enayet329 <mdsakayethossen2@gmail.com>
2024-08-12 17:51:01 +0600

    feat(collecton.model): add model for add collection

--- src/app/core/model/collection.mode..ts
+++ src/app/core/model/collection.mode..ts
@@ -1,13 +1,19 @@
-
 export interface Collection {
-createdDate: string|number|Date;
-    id: string;
-    name: string;
-    description: string;
-    topic: string;
-    imageUrl: string;
-    userId: string;
-    userName: string;
-    itemCount: number;
-  }
-  
+  createdDate: string | number | Date;
+  id: string;
+  name: string;
+  description: string;
+  topic: string;
+  imageUrl: string;
+  userId: string;
+  userName: string;
+  itemCount: number;
+}
+
+export interface AddCollectionRequest {
+  name: string;
+  description: string;
+  topic: string;
+  imageUrl: string;
+  userId: string;
+}
