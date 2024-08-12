cfcead8b546a0b51b4207e18744390fb0bf39f99
enayet329 <mdsakayethossen2@gmail.com>
2024-08-12 10:24:57 +0600

    feat(collection-detail): add collection detail component router navigate to item detail page

--- src/app/features/collections/collection-list/collection-list.component.ts
+++ src/app/features/collections/collection-list/collection-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Collection } from '../../../core/model/collection.mode.';
 import { CollectionService } from '../../../core/services/collection.service';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-collection-list',
@@ -12,13 +12,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './collection-list.component.css',
 })
 export class CollectionListComponent implements OnInit {
+
   collections: Collection[] = [];
 
-  constructor(private collectionService: CollectionService) {}
+  constructor(private collectionService: CollectionService,    private router: Router) {}
 
   ngOnInit(): void {
     this.collectionService.getCollections().subscribe((data) => {
       this.collections = data;
     });
   }
+
+  goToCollectionDetails(collectionId: string) {
+    this.router.navigate(['/collection-detail', collectionId]);
+    }
 }
