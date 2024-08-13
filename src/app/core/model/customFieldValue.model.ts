fb1b29b3f7bbecf0b9d995bbc8476c02d8e86a0c
enayet329 <mdsakayethossen2@gmail.com>
2024-08-13 14:15:29 +0600

    feat(customFieldValue): add custom field value response model

--- src/app/core/model/customFieldValue.model.ts
+++ src/app/core/model/customFieldValue.model.ts
@@ -3,3 +3,10 @@ export interface CustomFieldValue {
     customFieldId: string;
     itemId: string;
 }
+
+export interface CustomFieldValueResponse {
+    id: string;
+    value: string;
+    customFieldId: string;
+    itemId: string;
+}
