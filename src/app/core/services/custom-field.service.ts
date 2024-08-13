e801d3d2752deae0d3d1a7be5bfd8c66d1706368
enayet329 <mdsakayethossen2@gmail.com>
2024-08-13 11:23:22 +0600

    feat(custom-field.service): add custom field service for add new customfield and update customfield

--- src/app/core/services/custom-field.service.ts
+++ src/app/core/services/custom-field.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CustomField } from '../model/customField.model';
+import { CustomFieldConstants } from '../constants/constants';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomFieldService {
+  private apiUrl = CustomFieldConstants.API_ENDPOINTS;
+  httpClient = inject(HttpClient);
+  addCustomField(customField: CustomField[]): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+    });
 
-  constructor() { }
+    return this.httpClient.post<any>(this.apiUrl.ADD_CUSTOM_FIELD, customField, { headers });
+  }
+
+  updateCustomField(customField: CustomField[]): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+    });
+
+    return this.httpClient.put<any>(this.apiUrl.UPDATE_CUSTOM_FIELD, customField, { headers });
+  }
 }
