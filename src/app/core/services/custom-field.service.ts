b1aa3827ac4232c64691dc8aee9356cf93632f14
enayet329 <mdsakayethossen2@gmail.com>
2024-08-14 19:53:13 +0600

    feat(customField.Srvice): add a method for featch customField data

--- src/app/core/services/custom-field.service.ts
+++ src/app/core/services/custom-field.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { CustomField } from '../model/customField.model';
+import { CustomField, CustomFieldResponse } from '../model/customField.model';
 import { CustomFieldConstants } from '../constants/constants';
 import { Observable } from 'rxjs';
 
@@ -10,6 +10,7 @@ import { Observable } from 'rxjs';
 export class CustomFieldService {
   private apiUrl = CustomFieldConstants.API_ENDPOINTS;
   httpClient = inject(HttpClient);
+
   addCustomField(customField: CustomField[]): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -28,4 +29,10 @@ export class CustomFieldService {
 
     return this.httpClient.put<any>(this.apiUrl.UPDATE_CUSTOM_FIELD, customField, { headers });
   }
+
+  getCustomFieldsByCollectionId(collectionId: string): Observable<CustomFieldResponse[]> {
+    const url = `${this.apiUrl.GET_CUSTOM_FIELDS_BY_COLLECTION_ID}?collectionId=${collectionId}`;
+    return this.httpClient.get<CustomFieldResponse[]>(url);
+  }
+  
 }
