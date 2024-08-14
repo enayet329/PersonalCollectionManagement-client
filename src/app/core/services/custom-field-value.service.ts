7a50d1ff6f5053a5acb904144e6e206d7967a4cc
enayet329 <mdsakayethossen2@gmail.com>
2024-08-14 11:23:03 +0600

    feat(customFieldValue): add method for add customFieldValue and get cusomFieldValue by itemId

--- src/app/core/services/custom-field-value.service.ts
+++ src/app/core/services/custom-field-value.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CustomFieldValueConstants } from '../constants/constants';
+import { CustomFieldValue, CustomFieldValueResponse } from '../model/customFieldValue.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomFieldValueService {
 
-  constructor() { }
+  private apiurl = CustomFieldValueConstants.API_ENDPOINTS;
+
+  http = inject(HttpClient);
+
+  addCustomFieldValue(customFiledValue: CustomFieldValue[]): Observable<any>{
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+    }); 
+    return this.http.post(this.apiurl.ADD_CUSTOM_FIELD_VALUE, customFiledValue, {headers});
+  }
+
+  getCustomFieldValueByItemId(itemId: string): Observable<CustomFieldValueResponse[]> {
+    const params = { itemId: itemId };
+    return this.http.get<CustomFieldValueResponse[]>(this.apiurl.GET_CUSTOM_FIELD_VALUES_BY_ITEM_ID, { params });
+  }
+  
 }
