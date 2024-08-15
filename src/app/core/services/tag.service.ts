a695f4eb23e8b0023007c358a3ad2f62ea6dd61c
enayet329 <mdsakayethossen2@gmail.com>
2024-08-15 12:23:40 +0600

    feat(tag.service): add method for add tags and get all tags

--- src/app/core/services/tag.service.ts
+++ src/app/core/services/tag.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { TagConstants } from '../constants/constants';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AddTagRequest, AddTagResponse } from '../model/tag.model';
+import { Observable } from 'rxjs';
+import { ResponseModel } from '../model/response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  private api = ta
+  private api = TagConstants.API_ENDPOINTS;
+
+  http = inject(HttpClient);
+
+  getAllTags() {
+    return this.http.get<AddTagResponse[]>(this.api.GET_TAGS);
+  }
+
+  addTags(tags: AddTagRequest[]): Observable<ResponseModel> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'accept': 'text/plain',
+    });
+
+    return this.http.post<ResponseModel>(this.api.ADD_TAG, tags, { headers });
+  }
+
 }
