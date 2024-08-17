da3d852e785eb48fcc91041a8331cd6ecf85652f
enayet329 <mdsakayethossen2@gmail.com>
2024-08-17 13:02:06 +0600

    feat(customField.model): add update custom field model

--- src/app/core/services/custom-field-value.service.ts
+++ src/app/core/services/custom-field-value.service.ts
@@ -26,5 +26,21 @@ export class CustomFieldValueService {
     const params = { itemId: itemId };
     return this.http.get<CustomFieldValueResponse[]>(this.apiurl.GET_CUSTOM_FIELD_VALUES_BY_ITEM_ID, { params });
   }
+
+  updateCustomFieldValue(customFieldValue: CustomFieldValue): Observable<any>{
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+    }); 
+    return this.http.put(this.apiurl.UPDATE_CUSTOM_FIELD_VALUE, customFieldValue, {headers});
+  }
+
+  deleteCustomFieldValue(customFieldValueId: string): Observable<any>{
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+    }); 
+    return this.http.delete(this.apiurl.DELETE_CUSTOM_FIELD_VALUE + customFieldValueId, {headers});
+  }
   
 }
