a59ffbe09413479e18b94020f609c85b325c4a1b
enayet329 <mdsakayethossen2@gmail.com>
2024-08-20 11:50:17 +0600

    feat(auth.interceptor): Implemented HTTP interceptor for authentication, including token refresh logic upon 401 Unauthorized responses

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,5 +1,46 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+
+  const authService = inject(AuthService);
+
+  const token = localStorage.getItem('token');
+
+  const authReq = token
+    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+    : req;
+
+  return next(authReq).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+
+        return authService.getRefreshToken().pipe(
+          switchMap((newTokens: any) => {
+
+            localStorage.setItem('token', newTokens.accessToken);
+            localStorage.setItem('refreshToken', newTokens.refreshToken);
+
+
+            const newReq = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${newTokens.accessToken}`,
+              },
+            });
+
+            return next(newReq);
+          }),
+          catchError((refreshError) => {
+            localStorage.removeItem('token');
+            localStorage.removeItem('refreshToken');
+            console.error('Refresh token failed', refreshError);
+            return throwError(refreshError);
+          })
+        );
+      }
+      return throwError(error);
+    })
+  );
 };
